6ca4f54e24d3315d88710bb57555f1c87880a789
KZaczkiewicz <zaczkiewiczklaudia@gmail.com>
2022-06-26 17:16:11 +0200

    Nagłówki pliku .bmp

--- Projekt_v.cpp
+++ Projekt_v.cpp
@@ -1,6 +1,38 @@
 ﻿#include <iostream>
 #include <fstream>
+#include <stdio.h>
 using namespace std;
+
+//Nagłówek pliku
+struct FileHeader {
+    short bfType;
+    int bfSize;
+    short bfReserved1;
+    short bfReserved2;
+    int bfOffBits;
+} File;
+
+//Nagłówek obrazu
+struct BitMapInfoHeader {
+    int biSize;
+    int biWidth;
+    int biHeight;
+    short biPlanes;
+    short biBitCount;
+    int biCompression;
+    int biSizeImage;
+    int biXPelsPerMeter;
+    int biYPelsPerMeter;
+    int biClrUsed;
+    int biClrImportant;
+} Picture;
+
+//Paleta kolorów
+struct RGBColor {
+    char R;
+    char G;
+    char B;
+} Rgb;
 
 
 
